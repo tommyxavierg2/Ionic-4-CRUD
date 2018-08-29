f6c035d4324f134aa70d9499a3eb2e3b0e4fd97c
tommyxavierg2 <tommyxavierg@gmail.com>
2018-08-28 23:59:48 -0400

    Added fully signup page

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/user/auth.service';
+import { AlertController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.page.scss'],
 })
 export class ResetPasswordPage implements OnInit {
-
-  constructor() { }
+public resetPasswordForm: FormGroup;
+  constructor(private authService: AuthService, private alertCtrl: AlertController, private formBuilder: FormBuilder,
+    private router: Router) {
+      this.resetPasswordForm = this.formBuilder.group({
+        email: [
+          '',
+          Validators.compose([Validators.required, Validators.email]),
+        ],
+      });
+     }
 
   ngOnInit() {
+  }
+
+  resetPassword(resetPasswordForm: FormGroup): void {
+    if (!resetPasswordForm.valid) {
+      console.log('Form is not valid yet, current value:', resetPasswordForm.value);
+    } else {
+      const email: string = resetPasswordForm.value.email;
+      this.authService.resetPassword(email)
+        .then( async () => {
+          const alert = await this.alertCtrl.create({
+            message: 'Check your email for a password reset link',
+            buttons: [
+              {
+                text: 'Ok',
+                role: 'cancel',
+                handler: () => {
+                  this.router.navigateByUrl('login');
+                },
+              },
+            ],
+          });
+          await alert.present();
+        });
+    }
   }
 
 }
