5e6fac0bd2847bbfd35e66311e4631607d165c75
tommyxavierg2 <tommyxavierg@gmail.com>
2018-08-28 22:17:05 -0400

    Modified app general modules and added capacitor, app config, pages and services folders

---
+++ src/app/config/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyB1E5nNNNe26VKVSNYbdccFRjerMtByBS4',
+    authDomain: 'eventmanager-d78aa.firebaseapp.com',
+    databaseURL: 'https://eventmanager-d78aa.firebaseio.com',
+    projectId: 'eventmanager-d78aa',
+    storageBucket: 'eventmanager-d78aa.appspot.com',
+    messagingSenderId: '394446007468'
+};
---
+++ src/app/services/user/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  loginUser(email: string, password: string): Promise<any> {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+  signUpUser(email: string, password: string): Promise<any> {
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(newUserCredentials => {
+        firebase.database()
+          .ref(`/userProfile/${newUserCredentials.user.uid}/email`)
+          .set(email);
+      })
+      .catch(error => {
+        console.error(error);
+        throw new Error(error);
+      });
+  }
+
+  resetPassword(email: string): Promise<void> {
+    return firebase.auth().sendPasswordResetEmail(email);
+  }
+
+  logoutUser(): Promise<void> {
+    const userId: string = firebase.auth().currentUser.uid;
+    firebase.database()
+      .ref(`/userProfile/${userId}`)
+      .off();
+     return firebase.auth().signOut();
+  }
+}
